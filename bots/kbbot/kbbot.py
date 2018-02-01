cb15e7b793db1eb5fbcd368324da446fe352a7f1
baohien97 <tengsuynh@gmail.com>
2018-02-01 23:34:49 +0100

    put files in right folder

--- bots/kbbot/kbbot.py
+++ bots/kbbot/kbbot.py
@@ -7,11 +7,12 @@ pick a Jack to play whenever this is a legal move.
 It loads general information about the game, as well as the definition of a strategy,
 from load.py.
 """
-
-from api import State, util
+import numpy as np
+from api import State, Deck, util
 import random, load
 
 from kb import KB, Boolean, Integer
+from alphabeta import Bot as AlphaBeta
 
 class Bot:
 
@@ -25,16 +26,43 @@ class Bot:
         random.shuffle(moves)
 
         for move in moves:
+        #'''apply the knowledge base for the first phase of the game.For phase two alpha beta is applied'''
 
-            if not self.kb_consistent(state, move):
-                # Plays the first move that makes the kb inconsistent. We do not take
-                # into account that there might be other valid moves according to the strategy.
-                # Uncomment the next line if you want to see that something happens.
-                # print "Strategy Applied"
+            if state.get_phase() == 1: #apply knowledge base
+                # stock = np.array(Deck.get_stock())  # get card indexes still in stock
+                # # alternatively this could be be also estimated with make_estimation()
+                # hand = np.array(Deck.get_player_hand(1))
+                # # arrays with kings and queens
+                # kings = np.array([2, 7, 17, 18])
+                # queens = np.array([3, 8, 13, 18])
+                # # arrays with kings and queens in hand respectively
+                # kings_hand = np.intersect1d(hand, kings)
+                # queens_hand = np.intersect1d(hand, queens)
+                #
+                # # array with cards having matching partners in stock
+                # kings_partner = np.intersect1d((kings_hand + 1), stock) - 1
+                # queens_partner = np.intersect1d((queens_hand - 1), stock) + 1
+                #
+                # # stores all cards that should be saved for marriage
+                # save_card = np.append(kings_partner, queens_partner)
+                #
+                # if move[1] in save_card:
+                #     pass
+
+                if not self.kb_consistent(state, move):
+                    # Plays the first move that makes the kb inconsistent. We do not take
+                    # into account that there might be other valid moves according to the strategy.
+                    # Uncomment the next line if you want to see that something happens.
+                    # print "Strategy Applied"
+                    return move
+                # If no move that is entailed by the kb is found, play random move
+                return random.choice(moves)
+            
+            if state.get_phase() == 2: #apply alphabeta
+                alphabeta_bot = AlphaBeta()
+                val, move = alphabeta_bot.value(state)
                 return move
 
-        # If no move that is entailed by the kb is found, play random move
-        return random.choice(moves)
 
     # Note: In this example, the state object is not used,
     # but you might want to do it for your own strategy.
@@ -48,10 +76,13 @@ class Bot:
         load.general_information(kb)
 
         # Add the necessary knowledge about the strategy
-        if state.__leads_turn:
-            load.strategy_marriages()
-        else:
-            load.strategy_knowledge(kb)
+        # if state.__leads_turn:
+        #     load.strategy_knowledge(kb)
+        #   #  load.strategy_marriages(kb)
+        # else:
+
+        load.strategy_marriages(kb)
+        load.strategy_cheapcard(kb)
 
         # This line stores the index of the card in the deck.
         # If this doesn't make sense, refer to _deck.py for the card index mapping
